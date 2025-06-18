c5fb457ae29c4f1cf0e346f2da1e1f1fd751bbae
André C. Scherrer <andrescherrer@gmail.com>
2025-06-18 14:30:44 -0300

    feat: update DatabaseSeeder with model factory methods

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        \App\Models\Category::factory(5)->create();
+        \App\Models\Brand::factory(5)->create();
+        \App\Models\Product::factory(50)->create();
     }
 }
